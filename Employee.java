b9061933d5bef1fa653666045482aac066125501
dimashmyrev1-ux <dimashmyrev1@gmail.com>
2026-07-08 15:18:55 +0300

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,4 +1,6 @@
-public class Employee implements Payable {
+public class Employee implements Payable
+
+{
     @Override
     public void calculatePayment(int rate, int hours) {
         double Paycheck = rate * hours * 0.55;
